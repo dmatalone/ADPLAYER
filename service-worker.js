6d0879c20668da86c74c49dfff67a2128bea6172
dmatalone <matalonedominic2012@gmail.com>
2026-09-12 15:20:08 -0500

    Add service worker for standalone ESPN Fantasy launcher

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = "espn-fantasy-launcher-v1";
+const APP_FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./apple-touch-icon.png",
+  "./fantasy-ad.mp4"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
